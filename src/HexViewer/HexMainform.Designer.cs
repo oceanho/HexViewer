fa9c9fe3800656ffe9125a62a2e69f3a06f6f264
莴笋Ho <gzhehai@foxmail.com>
2015-11-30 12:14:49 +0800

    initial commit

---
+++ src/HexViewer/HexMainform.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace HexViewer
+{
+    partial class HexMainform
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Select_file = new System.Windows.Forms.Button();
+            this.tb_filepath = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_CopyHex = new System.Windows.Forms.Button();
+            this.btn_SelectHex = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Select_file);
+            this.groupBox1.Controls.Add(this.tb_filepath);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(578, 49);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btn_Select_file
+            // 
+            this.btn_Select_file.Location = new System.Drawing.Point(496, 18);
+            this.btn_Select_file.Name = "btn_Select_file";
+            this.btn_Select_file.Size = new System.Drawing.Size(74, 23);
+            this.btn_Select_file.TabIndex = 1;
+            this.btn_Select_file.Text = "选择文件";
+            this.btn_Select_file.UseVisualStyleBackColor = true;
+            this.btn_Select_file.Click += new System.EventHandler(this.btn_Select_file_Click);
+            // 
+            // tb_filepath
+            // 
+            this.tb_filepath.Location = new System.Drawing.Point(12, 19);
+            this.tb_filepath.Name = "tb_filepath";
+            this.tb_filepath.Size = new System.Drawing.Size(479, 21);
+            this.tb_filepath.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(578, 176);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_CopyHex
+            // 
+            this.btn_CopyHex.Location = new System.Drawing.Point(435, 262);
+            this.btn_CopyHex.Name = "btn_CopyHex";
+            this.btn_CopyHex.Size = new System.Drawing.Size(75, 27);
+            this.btn_CopyHex.TabIndex = 2;
+            this.btn_CopyHex.Text = "复制Hex";
+            this.btn_CopyHex.UseVisualStyleBackColor = true;
+            this.btn_CopyHex.Click += new System.EventHandler(this.btn_CopyHex_Click);
+            // 
+            // btn_SelectHex
+            // 
+            this.btn_SelectHex.Location = new System.Drawing.Point(516, 262);
+            this.btn_SelectHex.Name = "btn_SelectHex";
+            this.btn_SelectHex.Size = new System.Drawing.Size(75, 27);
+            this.btn_SelectHex.TabIndex = 2;
+            this.btn_SelectHex.Text = "选中Hex";
+            this.btn_SelectHex.UseVisualStyleBackColor = true;
+            this.btn_SelectHex.Click += new System.EventHandler(this.btn_SelectHex_Click);
+            // 
+            // HexMainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 302);
+            this.Controls.Add(this.btn_SelectHex);
+            this.Controls.Add(this.btn_CopyHex);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "HexMainform";
+            this.Text = ".NET File Hex String Viewer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tb_filepath;
+        private System.Windows.Forms.Button btn_Select_file;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_CopyHex;
+        private System.Windows.Forms.Button btn_SelectHex;
+    }
+}
+
---
+++ src/HexViewer/HexMainform.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HexViewer
+{
+    public partial class HexMainform : Form
+    {
+        public HexMainform()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Select_file_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Multiselect = false;
+                ofd.FileOk += ofd_FileOk;
+                ofd.ShowDialog();
+            }
+        }
+
+        void ofd_FileOk(object sender, CancelEventArgs e)
+        {
+            tb_filepath.Text = (sender as OpenFileDialog).FileName;
+            GenerateHexString();
+        }
+
+        private void GenerateHexString()
+        {
+            string path = tb_filepath.Text;
+            if (!File.Exists(path))
+                MessageBox.Show("出大事了", "我干，路径文件不存在", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            byte[] bytes = File.ReadAllBytes(path);
+            StringBuilder hexSb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                hexSb.Append(Convert.ToString(bytes[i], 16).ToUpper().PadLeft(2, '0'));
+            this.richTextBox1.Text = "0x" + hexSb.ToString();
+            bytes = null;
+        }
+
+        private void btn_CopyHex_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(this.richTextBox1.Text);
+        }
+
+        private void btn_SelectHex_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Focus();
+            richTextBox1.SelectAll();
+        }
+    }
+}
